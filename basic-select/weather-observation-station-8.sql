3b5a940618b2621da70cd5194ebe7c3215f9cee9
Vinicius Brasil <marcosbrasilaraujo@gmail.com>
2017-11-25 11:31:34 -0200

    Create weather-observation-station-8.sql

---
+++ basic-select/weather-observation-station-8.sql
@@ -0,0 +1,8 @@
+-- Weather Observation Station 8
+-- Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+-- https://www.hackerrank.com/challenges/weather-observation-station-8/problem
+
+SELECT DISTINCT City
+FROM Station
+WHERE LEFT(City, 1) IN ('a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U') AND
+      RIGHT(City, 1) IN ('a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U');
