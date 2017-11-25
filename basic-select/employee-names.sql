0ca515bc5f08927b11f471ac96a9ec1a403f6677
Vinicius Brasil <marcosbrasilaraujo@gmail.com>
2017-11-25 14:13:33 -0200

    Create employee-names.sql

---
+++ basic-select/employee-names.sql
@@ -0,0 +1,7 @@
+-- Employee Names
+-- Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+-- https://www.hackerrank.com/challenges/name-of-employees/problem
+
+SELECT Name
+FROM Employee
+ORDER BY Name;
