67b93527fc32723216fe358741fee80f68c2d4c5
Vinicius Brasil <marcosbrasilaraujo@gmail.com>
2017-11-26 12:35:38 -0200

    update to generic count

--- advanced-select/the-pads.sql
+++ advanced-select/the-pads.sql
@@ -10,7 +10,7 @@ SELECT CONCAT(Name, '(', LEFT(Occupation, 1) , ')')
 FROM Occupations
 ORDER BY Name;
 
-SELECT CONCAT('There are a total of ', COUNT(Occupation), ' ', LOWER(Occupation), 's.')
+SELECT CONCAT('There are a total of ', COUNT(*), ' ', LOWER(Occupation), 's.')
 FROM Occupations
 GROUP BY Occupation
 ORDER BY COUNT(Occupation) ASC, Occupation;
