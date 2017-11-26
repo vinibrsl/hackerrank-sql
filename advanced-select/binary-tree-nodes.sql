b4fc69aab899460896a8bf965154f03042909397
Vinicius Brasil <marcosbrasilaraujo@gmail.com>
2017-11-26 13:06:46 -0200

    delete description

--- advanced-select/binary-tree-nodes.sql
+++ advanced-select/binary-tree-nodes.sql
@@ -1,9 +1,4 @@
 -- Binary Tree Nodes
--- You are given a table, BST, containing two columns: N and P, where N represents the value of a node in Binary Tree, and P is the parent of N.
-   Write a query to find the node type of Binary Tree ordered by the value of the node. Output one of the following for each node:
-   Root: If node is root node.
-   Leaf: If node is leaf node.
-   Inner: If node is neither root nor leaf node.
 -- https://www.hackerrank.com/challenges/binary-search-tree-1/problem
 
 SELECT N, CASE
--- advanced-select/type-of-triangle.sql
+++ advanced-select/type-of-triangle.sql
@@ -1,9 +1,4 @@
 -- Type of Triangle
--- Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
-   Equilateral: It's a triangle with  sides of equal length.
-   Isosceles: It's a triangle with  sides of equal length.
-   Scalene: It's a triangle with  sides of differing lengths.
-   Not A Triangle: The given values of A, B, and C don't form a triangle.
 -- https://www.hackerrank.com/challenges/what-type-of-triangle/problem
 
 SELECT
