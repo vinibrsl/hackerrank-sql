1166001e6f246682d65e24d4a37f03295b797b2b
Vinicius Brasil <marcosbrasilaraujo@gmail.com>
2017-11-26 13:01:01 -0200

    delete description

--- advanced-select/the-pads.sql
+++ advanced-select/the-pads.sql
@@ -1,9 +1,4 @@
 -- The PADS
--- Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
-   Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them in the following format: 
-   There are a total of [occupation_count] [occupation]s.
-   where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS and [occupation] is the lowercase occupation name. If more than one Occupation has the same [occupation_count], they should be ordered alphabetically.
-   Note: There will be at least two entries in the table for each type of occupation.
 -- https://www.hackerrank.com/challenges/the-pads/problem
 
 SELECT CONCAT(Name, '(', LEFT(Occupation, 1) , ')')
