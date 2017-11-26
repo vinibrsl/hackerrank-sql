954d4c2c6d3b5a8164ba55dc15262f2742000f4b
Vinicius Brasil <marcosbrasilaraujo@gmail.com>
2017-11-26 13:14:02 -0200

    add Aggregation folder

---
+++ aggregation/revising-aggregation-the-count-function.sql
@@ -0,0 +1,6 @@
+-- Revising Aggregations - The Count Function
+-- https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem
+
+SELECT COUNT(*)
+FROM City
+WHERE Population > 100000;
