b429bfe98b8654d0e8b78e421c82a7eb2e2af2c1
Vinicius Brasil <vini@hey.com>
2022-02-19 19:42:37 +0000

    Update contest_leaderboard.sql

--- basic-join/contest_leaderboard.sql
+++ basic-join/contest_leaderboard.sql
@@ -1,11 +1,28 @@
 -- Contest Leaderboard
 -- https://www.hackerrank.com/challenges/contest-leaderboard/problem
 
-select * from 
-(select hackers.hacker_id, name, sum(subs.maxscore) as totalscore from hackers INNER JOIN 
-(select hacker_id, challenge_id, max(score) as maxscore from submissions
-group by hacker_id, challenge_id) subs
-on hackers.hacker_id=subs.hacker_id
-group by hackers.hacker_id, hackers.name) data 
-where data.totalscore > 0
-order by data.totalscore desc, hacker_id;
+select
+  hackers.hacker_id,
+  hackers.name,
+  SUM(scores.max_score)
+from
+  (
+    select
+      hacker_id,
+      challenge_id,
+      MAX(score) AS max_score
+    from
+      submissions
+    group by
+      challenge_id,
+      hacker_id
+  ) AS scores
+  inner join hackers ON scores.hacker_id = hackers.hacker_id
+group by
+  hackers.hacker_id,
+  hackers.name
+having
+  sum(scores.max_score) > 0
+order by
+  SUM(scores.max_score) DESC,
+  hackers.hacker_id ASC;
